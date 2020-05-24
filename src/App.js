e14731283d6a3e3f5470451f7b05cc3ab7c678fe
stetdawg <stetdawg101@gmail.com>
2020-05-23 19:28:24 -0700

    architechture

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import Landing from './comp/Landing.js'
 import './css/App.css';
 import './css/structure.css';
 
 import Shop from './comp/Shop';
 
 function App() {
-  return (
+  return (     
     <div className="App">
+    <Landing>
+      <h1>lol</h1>
+    </Landing>
       <main>
-        <section id="hero">
-          <div className="container">
-            <div className="content">
-              <div className="flex">
-                <div className="panel" style={{backgroundImage: 'url(https://cdn.discordapp.com/attachments/333107318835249152/708619198582226964/gambler-oversize-kevlar.png)'}}>
-                  <p>Basics</p>
-                </div>
-                <div className="panel" style={{backgroundImage: 'url(https://cdn.discordapp.com/attachments/333107318835249152/708619315821412392/yinhe-venus3.png)'}}>
-                  <p>Customs</p>
-                </div>
-                <div className="panel" style={{backgroundImage: 'url(https://cdn.discordapp.com/attachments/333107318835249152/708619382112124998/index2.png)'}}>
-                  <p>Accessories</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
         <section>
-          <Shop />
+
+         <Shop /> 
         </section>
       </main>
     </div>
--- src/comp/Shop.js
+++ src/comp/Shop.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import router from 'react-router'
 import ShopItem from './ShopItem';
-
 import merchandise from '../json/merchandise.json';
 
 
