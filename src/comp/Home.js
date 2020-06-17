101e67f53c46959bd4fbe5419efb48921c44039b
stetdawg <stetdawg101@gmail.com>
2020-06-16 22:29:55 -0700

    UX

--- src/comp/Home.js
+++ src/comp/Home.js
@@ -4,14 +4,13 @@ import Header from './Header'
 
 function Home(props) {
  return (
-        <div id="home">
-       <Header>
+        <div id="home">       
+         <div  className="contentPanel">
+         <Header>
        Welcome to Cole's Table Tennis!<br />
             Here you'll find a selection of products for sale at great prices, as well as reviews. There are also videos on gluing and cutting rubber, and applying topsheet onto sponge.<br/>
-           <b>Check out our current specials below:</b><br/>
+           <b>Check out our current specials below:</b>
        </Header>
-       
-         <div  className="contentPanel">
                 <Specials />
          </div>
         </div>
