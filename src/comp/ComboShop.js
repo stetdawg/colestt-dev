7769671de98f39ad5f329ce8247d3d1cfdfafdfd
aisbell <anthony.l.isbell@gmail.com>
2020-06-21 17:38:24 -0700

    gotta fix the cart a little, but we're workin on it

--- src/comp/ComboShop.js
+++ src/comp/ComboShop.js
@@ -15,8 +15,13 @@ class ShopCustom extends Component{
   constructor(props){
     super(props);
     this.state = {
-      didScroll: false,
-
+      // didScroll: false, // in case we need this later
+      bladePrice : null,
+      redRubberPrice : null,
+      blackRubberPrice : null,
+      handlesPrice: null,
+      casePrice: null,
+      comboPrice: null
     }
 
   }
@@ -57,16 +62,86 @@ class ShopCustom extends Component{
     */
 
 
-  handleChange = (event) => {
+  handleBladeChange(event){
 
-    for (let i = 0; i < event.target.children("option").length; i++){
-      //if option[i]
-      console.log(event.target.children("option").value);
-      //json[i].price
+    // console.log("The selected child is: " + event.target.selectedIndex);         // find the index of the selected child
+
+    // console.log("The price is: $" + Blades[event.target.selectedIndex].price);   // finds the price of the selected child
+    if (event.target.selectedIndex != 0){
+      this.setState({bladePrice : Blades[event.target.selectedIndex - 1].price}); // updates the state with that new value
+    } else {
+      this.setState({bladePrice : null});
+    }
+    // this.getTotal();
+  }
+
+
+  handleRedRubberChange(event){
+    if (event.target.selectedIndex != 0){
+      this.setState({redRubberPrice : Rubbers[event.target.selectedIndex - 1].price}); // updates the state with that new value
+    } else {
+      this.setState({redRubberPrice : null});
+    }
+    // this.getTotal();
+  }
+
+
+  handleBlackRubberChange(event){
+    if (event.target.selectedIndex != 0){
+      this.setState({blackRubberPrice : Rubbers[event.target.selectedIndex - 1].price}); // updates the state with that new value
+    } else {
+      this.setState({blackRubberPrice : null});
+    }
+    // this.getTotal();
+  }
+
+
+  handleHandleChange(event){
+    if (event.target.selectedIndex != 0){
+      this.setState({handlesPrice : Handles[event.target.selectedIndex - 1].price}); // updates the state with that new value
+    } else {
+      this.setState({handlesPrice : null});
+    }
+    // this.getTotal();
+  }
+
+
+  handleCaseChange(event){
+    if (event.target.selectedIndex != 0){
+      this.setState({casePrice : Cases[event.target.selectedIndex].price}); // updates the state with that new value
+    } else {
+      this.setState({casePrice : null});
+    }
+    // this.getTotal();
+  }
+
+
+  getTotal(){
+    const shipping = 2.99;
+    const total = this.state.bladePrice + this.state.redRubberPrice + this.state.blackRubberPrice + this.state.handlesPrice + this.state.casePrice;
+    // console.log("shipping: " + shipping);
+    console.log("The total is: " + total);
+    return total;
+  }
+
+  printTotal(){
+    return;
+  }
+
+  componentDidUpdate(){
+    if (this.state.bladePrice != null &&
+        this.state.redRubberPrice != null &&
+        this.state.blackRubberPrice != null &&
+        this.state.handlesPrice != null &&
+        this.state.casePrice != null)
+    {
+      console.log("The state is full");
+      this.getTotal();
+    } else {
+      console.log("The state IS NOT full");
     }
 
   }
-
 
   render() {
     return(
@@ -76,7 +151,8 @@ class ShopCustom extends Component{
         <div className="combo-shop">
           <div className="combo-item-container">
             <label for="Blade">Blade: </label>
-            <Select itemType="Blade" classN="blades" onChange={(e) => handleChange(e)}>
+            <Select itemType="Blade" classN="blades" onChange={(e) => this.handleBladeChange(e)}>
+              <option selected></option>
               {
                 Blades.map(item => <option>
                   {item.name + " ($" + item.price + ")"}
@@ -86,10 +162,11 @@ class ShopCustom extends Component{
           </div>
           <div className="combo-item-container">
             <label for="Red Rubber">Red Rubbber: </label>
-            <Select itemType="Red Rubber" classN="red-rubbers">
+            <Select itemType="Red Rubber" classN="red-rubbers" onChange={(e) => this.handleRedRubberChange(e)}>
+              <option selected></option>
               {
                 Rubbers.map(item => <option>
-                  {item.name}
+                  {item.name + " ($" + item.price + ")"}
                 </option>)
               }
             </Select>
@@ -100,10 +177,11 @@ class ShopCustom extends Component{
           </div>
           <div className="combo-item-container">
             <label for="Black Rubber">Black Rubbber: </label>
-            <Select itemType="Black Rubber" classN="black-rubbers">
+            <Select itemType="Black Rubber" classN="black-rubbers" onChange={(e) => this.handleBlackRubberChange(e)}>
+              <option selected></option>
               {
                 Rubbers.map(item => <option>
-                  {item.name}
+                  {item.name + " ($" + item.price + ")"}
                 </option>)
               }
             </Select>
@@ -114,25 +192,44 @@ class ShopCustom extends Component{
           </div>
           <div className="combo-item-container">
             <label for="Handles">Handles: </label>
-            <Select itemType="Handles" classN="handles">
+            <Select itemType="Handles" classN="handles" onChange={(e) => this.handleHandleChange(e)}>
+              <option selected></option>
               {
                 Handles.map(item => <option>
-                  {item.name}
+                  {item.name + " ($" + item.price + ")"}
                 </option>)
               }
             </Select>
           </div>
           <div className="combo-item-container">
             <label for="Case">Case: </label>
-            <Select itemType="Case" classN="case">
+            <Select itemType="Case" classN="case" onChange={(e) => this.handleCaseChange(e)}>
+              <option selected></option>
               {
                 Cases.map(item => <option>
-                  {item.name}
+                  {item.name + " ($" + item.price + ")"}
                 </option>)
               }
             </Select>
           </div>
-          <Button classN="">Buy Now</Button>
+          <div className="paddle-info">
+            <div className="info-container">
+              <label for="weight">Weight:</label>
+              <span name="weight">0.kg</span>
+            </div>
+            <div className="info-container">
+              <label for="speed">Speed:</label>
+              <span name="speed">Fast</span>
+            </div>
+            <div className="info-container">
+              <label for="shipping">Shipping:</label>
+              <span name="shipping">$2.99</span>
+              <input id="shipping" name="shipping" value={2.99} type="hidden" />
+            </div>
+          </div>
+          <Button classN="">
+            { this.getTotal() == null ? "Finish selecting your combo to generate your price" : "$" + this.getTotal() + " | Buy Now" }
+          </Button>
         </div>
       </div>
     )
