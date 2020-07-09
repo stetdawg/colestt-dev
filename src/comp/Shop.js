95eb55316f289d3e32e916cf05b5ba5fc131073e
stetdawg <stetdawg101@gmail.com>
2020-07-09 00:10:20 -0700

    .

--- src/comp/Shop.js
+++ src/comp/Shop.js
@@ -1,8 +1,7 @@
 //react
-import React from 'react'
+import React,{Fragment, useState, useEffect} from 'react'
 //import ReactDOM from 'react-dom'
 //import Router from 'react-router'
-
 import Filter from './Items/SearchFilter'
 import inventory from '../json/merchandise.json' 
 
@@ -13,57 +12,13 @@ import { addCart } from '../actions/addAction'
 // jsons
 import Merch from '../json/merchandise.json';
 
-const Filter = (props) =>{
-  const [radioType, onRadioSelect ] = useState("accessories")
-  const [categoryType, onRadioChange ] = useState(null) 
-  let lastRadioType = []
-  const arr = Object.keys(inventory)
-  const categoryKey = Object.entries(inventory)
-  const uniqueCategory = inventory[radioType]
-          .map(x => x.category)
-                   
-  let unique = [...new Set(uniqueCategory)]
-  useEffect(() => {
-      //pull from json, print to unordered list 
 
-     })
-          return <> 
-                     {arr.map(itemType => (
-              <Fragment key={itemType}>
-              <div className="container">
-                  <input 
-                  type="radio" 
-                  id="dept" 
-                  name="type" 
-                  onClick={() => onRadioSelect(itemType)}
-                  />
-                  <label htmlFor="dept">
-                      {itemType}
-                  </label>
-              </div> 
-              </Fragment>
-              ))      
-              }   
-              
-
-          <div className="department">
-              <div className="selection">
-              <p>{radioType}</p>
-                  <ul>
-                  {unique.map(x => { 
-                          return x ? <li> {x} </li> : null 
-                  })}
-                  </ul>
-              </div>
-          </div>
-          </>
-}
 
 
 
 //component to render a single item
 const Item = (i, a, itemCategory) => <li>
-{a[radioType]}
+{a["accessories"]}
 </li>
 //create a component to render item list
 console.log(inventory)
